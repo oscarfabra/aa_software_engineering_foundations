f4a5ae1963cd432c84a155205d59b5fa93deddfb
Oscar Fabra <oscarfabra@gmail.com>
2020-06-21 20:21:04 -0500

    Adds peak_finder and compress_str methods' solutions

--- algorithmic_strategies_exercise/05_compress_str.rb
+++ algorithmic_strategies_exercise/05_compress_str.rb
@@ -2,6 +2,20 @@
 # The method should return a new str where streaks of consecutive characters are compressed.
 # For example "aaabbc" is compressed to "3a2bc".
 
+def compress_str(str)
+  new_str = ""
+  i = 0
+  while i < str.length
+    char = str[i]
+    count = 0
+    while str[i] == char
+      i += 1
+      count += 1
+    end
+    new_str += (count > 1)? count.to_s + char : char
+  end
+  new_str
+end
 
 p compress_str("aaabbc")        # => "3a2bc"
 p compress_str("xxyyyyzz")      # => "2x4y2z"
