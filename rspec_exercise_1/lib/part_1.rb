e890dd943ac53c00ff460ea9f4783884954073d3
Oscar Fabra <oscarfabra@gmail.com>
2020-06-22 18:32:21 -0500

    Adds solutions to RSpec Exercise 1 - Part 1

---
+++ rspec_exercise_1/lib/part_1.rb
@@ -0,0 +1,34 @@
+# Returns the average of two numbers
+def average(n, m)
+  (n + m) / 2.0
+end
+
+# Returns the average of the numbers of the given array
+def average_array(arr)
+  arr.inject { |acc, n| acc + n } / arr.length.to_f
+end
+
+# Returns a new string where the original string is repeated n times
+def repeat(str, n)
+  new_str = ""
+  n.times { new_str += str }
+  new_str
+end
+
+# Returns a yelled version of the given string
+def yell(str)
+  str.upcase + "!"
+end
+
+# Returns the sentence with words alternating between uppercase and lowercase
+def alternating_case(sent)
+  new_arr = []
+  sent.split(" ").each_with_index do |word, i|
+    if i.even?
+      new_arr << word.upcase
+    else
+      new_arr << word.downcase
+    end
+  end
+  new_arr.join(" ")
+end
