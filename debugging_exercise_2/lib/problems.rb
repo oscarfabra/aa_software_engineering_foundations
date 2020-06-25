f4a944147dcaf500a200bd47e51cedc3470e9735
Oscar Fabra <oscarfabra@gmail.com>
2020-06-24 22:58:54 -0500

    Adds solutions to Debugging Exercise 2 problems

--- debugging_exercise_2/lib/problems.rb
+++ debugging_exercise_2/lib/problems.rb
@@ -1,3 +1,51 @@
-# Run `bundle exec rspec` and satisy the specs.
-# You should implement your methods in this file.
-# Feel free to use the debugger when you get stuck.
+# Returns the largest divisor of the number that is also a prime number
+def largest_prime_factor(n)
+  i = n
+  while i > 1
+    return i if n % i == 0 && prime?(i)
+    i -= 1
+  end
+end
+
+# Returns a boolean indicating whether or not the given number is prime
+def prime?(num)
+  return false if num < 2
+  (2...num).each do |factor|
+    return false if num % factor == 0
+  end
+  true
+end
+
+# Returns true when there are no duplicate characters in the string
+def unique_chars?(str)
+  hash = Hash.new(0)
+  str.each_char { |char| hash[char] += 1 }
+  hash.none? { |char, n| n > 1 }
+end
+
+# Returns an hash where keys are the elements that were repeated in the array 
+# and values are the indices where that element appears
+def dupe_indices(arr)
+  hash = Hash.new(0)
+  arr.each { |ele| hash[ele] += 1 }
+  repeated = hash.select { |ele, n| n > 1 }
+  new_hash = {}
+  repeated.keys.each { |ele| new_hash[ele] = indices_array(arr, ele) }
+  new_hash
+end
+
+# Returns an array with the indices where the given element appears
+def indices_array(arr, ele)
+  indices = []
+  arr.each_with_index { |e, i| indices << i if e == ele }
+  indices
+end
+
+# Returns true when the given arrays contain the same elements, in any order
+def ana_array(arr_1, arr_2)
+  hash_1 = Hash.new(0)
+  hash_2 = Hash.new(0)
+  arr_1.each { |ele| hash_1[ele] += 1 }
+  arr_2.each { |ele| hash_2[ele] += 1 }
+  hash_1 == hash_2
+end
