b038b7b4e14d2b489a940de2836135a92dd4761a
Oscar Fabra <oscarfabra@gmail.com>
2020-07-10 23:33:52 -0500

    Updates blocks_project - Part 2 solution

--- blocks_project/lib/part_2.rb
+++ blocks_project/lib/part_2.rb
@@ -1,21 +1,19 @@
 # Returns true when all words are properly capitalized
 def all_words_capitalized?(words)
-  words.all? { |word| word[0] == word[0].upcase && word[1..-1] == word[1..-1].downcase }
+  words.all? { |word| word.capitalize == word }
 end
 
 # Returns true when none of the urls end in '.com', '.net', '.io', or '.org'
 def no_valid_url?(urls)
+  valid_endings = ['.com', '.net', '.io', '.org']
   urls.none? do |url|
-    url[-4..-1] == ".com" ||
-    url[-4..-1] == ".net" ||
-    url[-3..-1] == ".io" ||
-    url[-4..-1] == ".org"
+    valid_endings.any? { |ending| url.end_with?(ending) }
   end
 end
 
 # Returns true when at least one student has an average grade of 75 or higher
 def any_passing_students?(students)
   students.any? do |student|
-    student[:grades].sum.to_f / student[:grades].length > 75
+    student[:grades].sum.to_f / student[:grades].length >= 75
   end
 end
