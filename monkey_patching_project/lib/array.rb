71fe2cf62d128f661a0116e44ae1f0cc85992876
Oscar Fabra <oscarfabra@gmail.com>
2020-07-23 13:33:40 -0500

    Adds solution to Part 1 of Monkey Patching Project

--- monkey_patching_project/lib/array.rb
+++ monkey_patching_project/lib/array.rb
@@ -1,4 +1,28 @@
 # Monkey-Patch Ruby's existing Array class to add your own custom methods
 class Array
-  
+  # Returns the difference between the largest and smallest element
+  def span
+    return nil if self.empty?
+    self.max - self.min
+  end
+
+  # Returns a number representing the average value of the elements
+  def average
+    return nil if self.empty?
+    self.sum.to_f / self.length
+  end
+
+  # Returns the median element of the array when sorted
+  def median
+    return nil if self.empty?
+    self.sort!
+    (length.odd?)? self[length / 2] : (self[length / 2 - 1] + self[length / 2]) / 2.0
+  end
+
+  # Returns a hash representing the count of each element of the array
+  def counts
+    count = Hash.new(0)
+    self.each { |ele| count[ele] += 1 }
+    count
+  end
 end
