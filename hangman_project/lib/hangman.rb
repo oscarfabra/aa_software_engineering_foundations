6f848c66661b39a53de7399e6c2a03925a2ed83c
Oscar Fabra <oscarfabra@gmail.com>
2020-07-24 13:33:21 -0500

    Adds solution to Part 1 of hangman_project

--- hangman_project/lib/hangman.rb
+++ hangman_project/lib/hangman.rb
@@ -1,4 +1,51 @@
 class Hangman
   DICTIONARY = ["cat", "dog", "bootcamp", "pizza"]
 
+  # Returns a random word in the dictionary
+  def self.random_word
+    DICTIONARY.sample
+  end
+
+  # Creates a new Hangman instance
+  def initialize
+    @secret_word = Hangman::random_word
+    @guess_word = Array.new(@secret_word.length) { |char| '_' }
+    @attempted_chars = []
+    @remaining_incorrect_guesses = 5
+  end
+
+  # Returns @guess_word
+  def guess_word
+    @guess_word
+  end
+
+  # Returns @attempted_chars
+  def attempted_chars
+    @attempted_chars
+  end
+
+  # Returns @remaining_incorrect_guesses
+  def remaining_incorrect_guesses
+    @remaining_incorrect_guesses
+  end
+
+  # Returns true if the given char is in @attempted_chars
+  def already_attempted?(char)
+    @attempted_chars.any? { |c| c.downcase == char.downcase }
+  end
+
+  # Returns an array containing all indices of @secret_word where the char
+  # can be found
+  def get_matching_indices(char)
+    indices = []
+    (0...@secret_word.length).each do |i|
+      indices << i if @secret_word[i] == char
+    end
+    indices
+  end
+
+  # Sets the given indices of @guess_word to the given char
+  def fill_indices(char, indices)
+    indices.each { |i| @guess_word[i] = char }
+  end
 end
