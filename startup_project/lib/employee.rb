dab2d717db7b6f2fcb122ff51da2e962c298a381
Oscar Fabra <oscarfabra@gmail.com>
2020-07-26 17:05:47 -0500

    Adds solution to Part 1 of startup_project

--- startup_project/lib/employee.rb
+++ startup_project/lib/employee.rb
@@ -1,3 +1,15 @@
 class Employee
+  attr_reader :name, :title
 
+  # Creates a new Employee instance
+  def initialize(name, title)
+    @name = name
+    @title = title
+    @earnings = 0
+  end
+
+  # Pays the employee
+  def pay(amount)
+    @earnings += amount
+  end
 end
