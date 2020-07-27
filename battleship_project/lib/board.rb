57ab546d78322c002436a24db43dc49ee77f3ab1
Oscar Fabra <oscarfabra@gmail.com>
2020-07-27 17:43:19 -0500

    Adds solution to Part 1 of battleship_project

--- battleship_project/lib/board.rb
+++ battleship_project/lib/board.rb
@@ -1,3 +1,24 @@
 class Board
+  attr_reader :size
+
+  # Creates a new Board instance
+  def initialize(n)
+    @grid = Array.new(n) { Array.new(n, :N) }
+    @size = n * n
+  end
   
+  # Returns the element of @grid at the given position
+  def [](pos)
+    @grid[pos[0]][pos[1]]
+  end
+
+  # Sets the given position of @grid to the given value
+  def []=(pos, value)
+    @grid[pos[0]][pos[1]] = value
+  end
+
+  # Returns a number representing the count of :S values in @grid
+  def num_ships
+    @grid.flatten.count(:S)
+  end
 end
