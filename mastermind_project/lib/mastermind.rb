2f49dfb166e3c8c5e6c56177c5ff81839535c2db
Oscar Fabra <oscarfabra@gmail.com>
2020-07-27 16:20:03 -0500

    Adds solution to Part 3 of mastermind_project

--- mastermind_project/lib/mastermind.rb
+++ mastermind_project/lib/mastermind.rb
@@ -1,5 +1,23 @@
 require_relative "code"
 
 class Mastermind
+  # Creates a new Mastermind instance
+  def initialize(length)
+    @secret_code = Code.random(length)
+  end
 
+  # Prints the number of exact matches and near matches between @secret_code
+  # and the given Code instance
+  def print_matches(guess)
+    puts "Exact matches: #{ @secret_code.num_exact_matches(guess) }"
+    puts "Near matches: #{ @secret_code.num_near_matches(guess) }"
+  end
+
+  # Asks the user for a guess and responds accordingly
+  def ask_user_for_guess
+    print "Enter a code: "
+    guess = Code.from_string(gets.chomp)
+    self.print_matches(guess)
+    @secret_code == guess
+  end
 end
