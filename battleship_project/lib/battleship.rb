022257d922bae7e46b2fc0acc63b03eda5b33db0
Oscar Fabra <oscarfabra@gmail.com>
2020-07-27 19:46:09 -0500

    Adds solution to Part 3 of battleship_project

--- battleship_project/lib/battleship.rb
+++ battleship_project/lib/battleship.rb
@@ -2,5 +2,47 @@ require_relative "board"
 require_relative "player"
 
 class Battleship
+  attr_reader :board, :player
 
+  # Creates a new Battleship instance
+  def initialize(n)
+    @player = Player.new
+    @board = Board.new(n)
+    @remaining_misses = @board.size / 2
+  end
+
+  # Randomly places ships on the @board and prints accordingly
+  def start_game
+    @board.place_random_ships
+    puts "There are #{@board.num_ships} hidden ships on the board."
+    @board.print
+  end
+
+  # Tells if the user has lost the game
+  def lose?
+    return false if @remaining_misses > 0
+    puts "You lose"
+    true
+  end
+
+  # Tells if the user has won the game
+  def win?
+    return false if @board.num_ships > 0
+    puts "You win!"
+    true
+  end
+
+  # Returns true if the game has been won or lost
+  def game_over?
+    self.lose? || self.win?
+  end
+
+  # Gets a move from the player and acts accordingly
+  def turn
+    pos = @player.get_move
+    @board.attack(pos)
+    @board.print
+    @remaining_misses -= 1 if board[pos] == :X
+    puts "#{@remaining_misses} remaining misses."
+  end
 end
--- battleship_project/lib/player.rb
+++ battleship_project/lib/player.rb
@@ -1,3 +1,8 @@
 class Player
-
+  # Gets a position with coordinates from the user
+  def get_move
+    print "enter a position with coordinates separated with a space like '4 7': "
+    coord = gets.chomp
+    coord.split.map(&:to_i)
+  end
 end
