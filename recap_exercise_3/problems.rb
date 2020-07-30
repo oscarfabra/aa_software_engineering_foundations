60adc9fa0f44ea9af33b4490ff65faacbdfcfe8c
Oscar Fabra <oscarfabra@gmail.com>
2020-07-30 18:58:35 -0500

    Adds solution to problems 10 to 12 of recap_exercise_3

--- recap_exercise_3/problems.rb
+++ recap_exercise_3/problems.rb
@@ -193,27 +193,59 @@ end
 # Returns the product of a and b, recursively and without using the multiplication
 # operator (*)
 def multiply(a, b)
-
+  return 0 if a == 0 || b == 0
+  return a + multiply(a, b - 1) if b > 0
+  return b + multiply(a - 1, b) if a > 0
+  return -a + multiply(a, b + 1) if a < 0 && b < 0
 end
+
+# p multiply(3, 5)        # => 15
+# p multiply(5, 3)        # => 15
+# p multiply(2, 4)        # => 8
+# p multiply(0, 10)       # => 0
+# p multiply(-3, -6)      # => 18
+# p multiply(3, -6)       # => -18
+# p multiply(-3, 6)       # => -18
 
 # The Lucas Sequence is a sequence of numbers. The first number of the sequence 
 # is 2. The second number of the Lucas Sequence is 1. To generate the next number
 # of the sequence, we add up the previous two numbers. For example, the first six
 # numbers of the sequence are: 2, 1, 3, 4, 7, 11, ...
-
+#
 # Accepts a number representing a length as an arg. The method returns an array 
 # containing the Lucas Sequence up to the given length. A recursive method.
-def lucasSequence(length)
-
+def lucas_sequence(length)
+  return [] if length == 0
+  return [2] if length == 1
+  return [2, 1] if length == 2
+  lucas_sequence(length - 1) + [lucas_sequence(length - 1)[-2] + lucas_sequence(length - 1)[-1]]
 end
+
+# p lucas_sequence(0)   # => []
+# p lucas_sequence(1)   # => [2]    
+# p lucas_sequence(2)   # => [2, 1]
+# p lucas_sequence(3)   # => [2, 1, 3]
+# p lucas_sequence(6)   # => [2, 1, 3, 4, 7, 11]
+# p lucas_sequence(8)   # => [2, 1, 3, 4, 7, 11, 18, 29]
 
 # The Fundamental Theorem of Arithmetic states that every positive integer is 
 # either a prime or can be represented as a product of prime numbers.
-
+#
 # Accepts a number and returns an array representing the prime factorization 
 # of the given number. This means that the array should contain only prime 
 # numbers that multiply together to the given num. The array returned should 
 # contain numbers in ascending order. A recursive method.
 def prime_factorization(num)
-
+  i = 2
+  i += 1 until num % i == 0
+  return [num] if num == i
+  [i] + prime_factorization(num / i)
 end
+
+# p prime_factorization(12)     # => [2, 2, 3]
+# p prime_factorization(24)     # => [2, 2, 2, 3]
+# p prime_factorization(25)     # => [5, 5]
+# p prime_factorization(60)     # => [2, 2, 3, 5]
+# p prime_factorization(7)      # => [7]
+# p prime_factorization(11)     # => [11]
+# p prime_factorization(2017)   # => [2017]
