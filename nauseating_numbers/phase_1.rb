57e38a1092ead740ce7cf9ffeaa369bcdaf96ee8
Oscar Fabra <oscarfabra@gmail.com>
2020-08-02 19:45:04 -0500

    Adds solution to Phase 1 of nauseating_numbers

--- nauseating_numbers/phase_1.rb
+++ nauseating_numbers/phase_1.rb
@@ -1,7 +1,11 @@
 # Returns a count of the number of distinct pairs of elements that have a sum of
 # zero. Assumes that the input array contains unique elements.
 def strange_sums(arr)
-
+  count = 0
+  (0...arr.length - 1).each do |i|
+    count += 1 if arr[i + 1..-1].include?(-arr[i])
+  end
+  count
 end
 
 # p strange_sums([2, -3, 3, 4, -2])     # 2
@@ -14,7 +18,10 @@ end
 # array result in the product when multiplied together. Assumes that the 
 # input array contains unique elements.
 def pair_product(arr, prod)
-
+  (0...arr.length - 1).each do |i|
+    return true if arr[i + 1..-1].include?(prod / arr[i])
+  end
+  false
 end
 
 # p pair_product([4, 2, 5, 8], 16)    # true
@@ -29,7 +36,10 @@ end
 # number of times specified by the hash. If a character does not exist as a key
 # of the hash, then it should remain unchanged.
 def rampant_repeats(str, hash)
-
+  arr = str.split("").map do |c|
+    (hash.has_key?(c))? c * hash[c] : c
+  end
+  arr.join
 end
 
 # p rampant_repeats('taco', {'a'=>3, 'c'=>2})             # 'taaacco'
@@ -42,15 +52,18 @@ end
 # itself. For example, since 64 = 8 * 8 and 144 = 12 * 12, 64 and 144 are 
 # perfect squares; 35 is not a perfect square.
 def perfect_square?(n)
-
+  i = 1
+  i += 1 while i * i <= n
+  i -= 1
+  i * i == n
 end
 
-# p perfect_square(1)     # true
-# p perfect_square(4)     # true
-# p perfect_square(64)    # true
-# p perfect_square(100)   # true
-# p perfect_square(169)   # true
-# p perfect_square(2)     # false
-# p perfect_square(40)    # false
-# p perfect_square(32)    # false
-# p perfect_square(50)    # false
+# p perfect_square?(1)     # true
+# p perfect_square?(4)     # true
+# p perfect_square?(64)    # true
+# p perfect_square?(100)   # true
+# p perfect_square?(169)   # true
+# p perfect_square?(2)     # false
+# p perfect_square?(40)    # false
+# p perfect_square?(32)    # false
+# p perfect_square?(50)    # false
