681b24f83f999c0cbe3763b1b680688417868bcf
Oscar Fabra <oscarfabra@gmail.com>
2020-08-09 19:31:03 -0500

    Completes solution of Phase 2 of todo_board

--- todo_board/todo_board.rb
+++ todo_board/todo_board.rb
@@ -3,8 +3,29 @@ require_relative 'list.rb'
 class TodoBoard
 
   # Creates a new TodoBoard instance
-  def initialize(label)
-    @list = List.new(label)
+  def initialize
+    @lists = {}
+  end
+
+  # Creates a new List with the given label and adds it to @lists
+  def add_list(label)
+    return false if @lists.keys.include?(label)
+    @lists[label] = List.new(label)
+    true
+  end
+
+  # Prints the labels of all the available lists
+  def print_labels
+    @lists.keys.each do |label|
+      puts " #{label}"
+    end
+  end
+
+  # Prints all lists
+  def print_lists
+    @lists.values.each do |list|
+      list.print
+    end
   end
 
   # Prompts the user to enter a command and performs the appropriate action
@@ -13,26 +34,32 @@ class TodoBoard
     print "\nEnter a command: "
     cmd, *args = gets.chomp.split(" ")
     case cmd
+    when 'mklist'
+      self.add_list(*args)
+    when 'ls'
+      self.print_labels
+    when 'showall'
+      self.print_lists
     when 'mktodo'
-      @list.add_item(*args)
-    when 'up'
-      (args[1].nil?)? @list.up(args[0].to_i) : @list.up(args[0].to_i, args[1].to_i)
-    when 'down'
-      (args[1].nil?)? @list.down(args[0].to_i) : @list.down(args[0].to_i, args[1].to_i)
-    when 'swap'
-      @list.swap(args[0].to_i, args[1].to_i)
-    when 'sort'
-      @list.sort_by_date!
-    when 'priority'
-      @list.print_priority
-    when 'print'
-      (args.length == 0)? @list.print : @list.print_full_item(args[0].to_i)
+      (args[3].nil?)? @lists[args[0]].add_item(args[1], args[2]) : @lists[args[0]].add_item(args[1], args[2], args[3])
     when 'toggle'
-      @list.toggle_item(args[0].to_i)
+      @lists[args[0]].toggle_item(args[1].to_i)
     when 'rm'
-      @list.remove_item(args[0].to_i)
+      @lists[args[0]].remove_item(args[1].to_i)
     when 'purge'
-      @list.purge
+      @lists[args[0]].purge
+    when 'up'
+      (args[2].nil?)? @lists[args[0]].up(args[1].to_i) : @lists[args[0]].up(args[1].to_i, args[2].to_i)
+    when 'down'
+      (args[2].nil?)? @lists[args[0]].down(args[1].to_i) : @lists[args[0]].down(args[1].to_i, args[2].to_i)
+    when 'swap'
+      @lists[args[0]].swap(args[1].to_i, args[2].to_i)
+    when 'sort'
+      @lists[args[0]].sort_by_date!
+    when 'priority'
+      @lists[args[0]].print_priority
+    when 'print'
+      (args[1].nil?)? @lists[args[0]].print : @lists[args[0]].print_full_item(args[1].to_i)
     when 'quit'
       return false
     else
@@ -47,5 +74,8 @@ class TodoBoard
     while self.get_command
     end
   end
-
 end
+
+# Creates and runs a new board for testing purposes
+b = TodoBoard.new
+b.run
